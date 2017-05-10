0271982b360d9e0762c0471ad62802ba5f07816c
Alweezy <alvinmutisya@gmail.com>
2017-05-10 21:36:03 +0300

    Initial commit

---
+++ boot-camp-18-day-1/prime_number_tests.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from prime_numbers import is_prime, collect_prime_numbers
+
+
+class PrimeNumberTests(TestCase):
+    """Check if prime number generator outputs prime numbers"""
+    def test_if_number_is_prime(self):
+        self.assertEqual(is_prime(1), False)
+
+    def test_if_number_is_prime2(self):
+        self.assertEqual(is_prime(7), True)
+
+
+class OutPutTests(TestCase):
+    """test if output is correct"""
+    def test_prime_numbers_correct(self):
+        self.assertEqual(collect_prime_numbers(13), [2, 3, 5, 7, 11, 13])
+
+    def test_prime_numbers_correct2(self):
+        self.assertEqual(collect_prime_numbers(2), [2])
+
+    def test_number_included_if_prime(self):
+        self.assertEqual(collect_prime_numbers(5), [2, 3, 5])
+
+    def test_input_is_not_string(self):
+        self.assertEqual(collect_prime_numbers(''), 'Only integers allowed')
+
+    def test_input_is_not_dictionary(self):
+        self.assertEqual(collect_prime_numbers({}), 'Only integers allowed')
+
+    def test_input_is_not_tuple(self):
+        self.assertEqual(collect_prime_numbers((1,)), 'Only integers allowed')
---
+++ boot-camp-18-day-1/prime_numbers.py
@@ -0,0 +1,20 @@
+def is_prime(number):
+    if number is 1:
+        return False
+    for value in range(2, int(number ** 0.5) + 1):
+        if number % value is 0:
+            return False
+        return True
+
+
+def collect_prime_numbers(number):
+    prime_numbers = []
+    if isinstance(number, int):
+        if number >= 2:
+            prime_numbers.append(2)
+        for i in range(2, number + 1):
+            if is_prime(i):
+                prime_numbers.append(i)
+        return prime_numbers
+    else:
+        return 'Only integers allowed'
